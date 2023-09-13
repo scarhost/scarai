165c16ddcaff31c1a3b5054e1618b5d00e4b8eeb
Priyansh <itspriyanshvats@gmail.com>
2023-09-12 19:45:04 -0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <form className="fixed inset-x-0 bottom-10" onSubmit={handleSubmit}>
         <input
           className="max-w-3xl shadow-xl w-full mx-auto py-8 flex h-10 rounded-md border border-input bg-background px-3 text-sm ring-offset-background"
-          placeholder="Send a message"
+          placeholder="Type Your Messeage Or Request to Scar-AI"
           value={input}
           onChange={handleInputChange}
         />
